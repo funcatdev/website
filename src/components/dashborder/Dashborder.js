f2b3a82739fdef1d451f156db48420d41f95aa68
zack <17081371097@163.com>
2018-11-21 07:57:59 +0800

    add aboutus

--- src/components/dashborder/Dashborder.js
+++ src/components/dashborder/Dashborder.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout ,Card, Col, Row} from 'antd';
+import { Layout,List,Card, Col, Row} from 'antd';
 
 
 const { Content } = Layout
@@ -11,7 +11,7 @@ const CardItem = ()=>(
     hoverable
     style={{ width: 240 }}
     cover={<img alt="example" src="//img3.mukewang.com/5a43337e0001160006000338-240-135.jpg" />}
-  >
+    >
     <Meta
       title="产品经理"
       description="最近在学：1660人"
@@ -19,6 +19,43 @@ const CardItem = ()=>(
   </Card>
 );
 
+const data = [
+    {
+      title: 'Title 1',
+    },
+    {
+      title: 'Title 2',
+    },
+    {
+      title: 'Title 3',
+    },
+    {
+      title: 'Title 4',
+    },
+  ];
+
+const CourseContent = ()=>(
+    <>
+    <h3>课程优势</h3>
+    <List
+        grid={{ gutter: 16, column: 4 }}
+        dataSource={data}
+        renderItem={item => (
+          <List.Item>
+            <Card title={item.title}>Card content</Card>
+          </List.Item>
+        )}
+      />
+    </>
+);
+
+
+const AboutUs = ()=>(
+    <>
+    <h3>关于我们</h3>
+    <p>凡猫教育在互联网教育和咨询行业深耕4年，主要业务和互联网培训和企业互联网转型咨询和服务，旨在打造互联网的人才生态圈。我们深知现在职场人士的迷茫，针对于学生不同性格和经历，针对学生设计不同的课程体系内容。已举办互联网行业分享沙龙200多场，培训学生3000多人进去了国内大中型的互联网公司，实现了转行梦。我们的使命是让所有转行者都能够轻松入行互联网行业，从事自己喜欢的事业同时获得可观的收入。</p>
+    </>
+);
 
 const Dashborder = () => {
     return (
@@ -43,9 +80,9 @@ const Dashborder = () => {
                         <CardItem />
                     </Col>
                 </Row>
-          
-                
             </div>
+            <CourseContent />
+            <AboutUs/>
         </Content>
     );
 }
